8861fa6d89d052ddf44de21c90e72553a4bcc84f
Dan McAulay <dmcaulay@gmail.com>
2014-03-18 16:58:35 -0700

    add an app_user cookbook

---
+++ app_user/recipes/default.rb
@@ -0,0 +1,29 @@
+#
+# Cookbook Name:: app_user
+# Recipe:: default
+#
+# Copyright 2014, Prix Fixe
+#
+# All rights reserved - Do Not Redistribute
+#
+
+user "app" do
+  comment "prix fixe app user"
+  home "/home/app"
+  shell "/bin/bash"
+end
+
+directory "/home/app/" do
+  owner "app"
+  mode "0755"
+  group "app"
+  action :create
+end
+
+directory "/home/app/.ssh" do
+  owner "app"
+  mode "0700"
+  group "app"
+  action :create
+end
+

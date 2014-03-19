8a35be2b0262413b02b06eaf33e718cef3834a26
Dan McAulay <dmcaulay@gmail.com>
2014-03-18 23:50:11 -0700

    wow. i guess that turned off port 22.

--- app_user/recipes/default.rb
+++ app_user/recipes/default.rb
@@ -11,6 +11,12 @@ firewall 'ufw' do
   action :enable
 end
 
+firewall_rule 'ssh' do
+  port     22
+  action   :allow
+  notifies :enable, 'firewall[ufw]'
+end
+
 firewall_rule "http" do
   port 80
   action :allow
